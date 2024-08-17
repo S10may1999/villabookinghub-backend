7a4f8895c063449316ba816e07a532e123cdc090
MMISProject <mmistechnologies@gmail.com>
2024-08-17 17:50:55 +0530

    token expiry added

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,104 +1,106 @@
 const jsonwebtoken = require("jsonwebtoken")
-const userModel=require("../schema/user-schema")
-const bcrypt=require("bcrypt")
+const userModel = require("../schema/user-schema")
+const bcrypt = require("bcrypt")
 
 
-const getFunction=(req,res)=>{
+const getFunction = (req, res) => {
     try {
 
         res.status(200).json({
-            message:"this is working"
+            message: "this is working"
         })
     } catch (error) {
         console.log(error)
     }
 }
 
-const profileUpload=async(req,res)=>{
-    if(!req.files || req.files.length==0) return res.status(404).json({
-        message:"No Files uploaded !!"
+const profileUpload = async (req, res) => {
+    if (!req.files || req.files.length == 0) return res.status(404).json({
+        message: "No Files uploaded !!"
     })
-    
-    const filePath=req.files.map((file)=>file.path)
 
-    const token=req.cookies.jwt
+    const filePath = req.files.map((file) => file.path)
 
-    const decode=jsonwebtoken.verify(token,process.env.JWT_SECREAT_KEY)
+    const token = req.cookies.jwt
 
-    const userId=decode.id
+    const decode = jsonwebtoken.verify(token, process.env.JWT_SECREAT_KEY)
 
-    await userModel.findByIdAndUpdate(userId,{profileImg:filePath.toString()},{new:true}).then((result) => {
+    const userId = decode.id
+
+    await userModel.findByIdAndUpdate(userId, { profileImg: filePath.toString() }, { new: true }).then((result) => {
         res.status(200).json({
-            message:"File Uploaded Successfully !!",
-            data:result.profileImg
+            message: "File Uploaded Successfully !!",
+            data: result.profileImg
         })
     }).catch((err) => {
         res.status(500).json({
-            message:"Something went wrong !!",
-            error:err.message
-        })        
+            message: "Something went wrong !!",
+            error: err.message
+        })
     });
-    
-    
+
+
 }
 
-const userSignIn=async(req,res)=>{
-    const {email,password}=req.body
+const userSignIn = async (req, res) => {
+    const { email, password } = req.body
     try {
-        const user= await userModel.findOne({email})
-        if(!user) return res.status(404).json({
-            message:"Incorrect email"
+        const user = await userModel.findOne({ email })
+        if (!user) return res.status(404).json({
+            message: "Incorrect email"
         })
 
-        const isPasswrodMatch=await bcrypt.compare(password,user.password)
+        const isPasswrodMatch = await bcrypt.compare(password, user.password)
 
-        if(!isPasswrodMatch) return res.status(400).json({
-            message:"Incorrect password !!"
+        if (!isPasswrodMatch) return res.status(400).json({
+            message: "Incorrect password !!"
         })
-        const token=jsonwebtoken.sign({"id":user._id},process.env.JWT_SECREAT_KEY,{"expiresIn":"1hr"})
-
-        res.cookie("jwt",token,{
-            httpOnly:true,
-            secure:process.env.NODE_ENV === 'production',
-            sameSite:"None"
+        const token = jsonwebtoken.sign({ "id": user._id }, process.env.JWT_SECREAT_KEY, { "expiresIn": "1hr" })
+        const expires = new Date();
+        expires.setFullYear(expires.getFullYear() + 10)
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: "None",
+            expires:expires
         })
 
         res.status(200).json({
-            message:"Logged In Successfully !!"
+            message: "Logged In Successfully !!"
         })
     } catch (error) {
-            console.log(error)        
+        console.log(error)
     }
 }
 
-const userRegister=async(req,res)=>{
-    const {firstName,lastName,mobileNumber,email,address,password}=req.body
+const userRegister = async (req, res) => {
+    const { firstName, lastName, mobileNumber, email, address, password } = req.body
     console.log(req.body)
-    if(!mobileNumber || !email || !password) return res.status(400).json({
-        message:"mobile , email and password required !!"
+    if (!mobileNumber || !email || !password) return res.status(400).json({
+        message: "mobile , email and password required !!"
     })
-    const enPassword=await bcrypt.hash(password,10)
-    await userModel.create({"firstName":firstName,"lastName":lastName,"mobileNumber":mobileNumber,"email":email,"address":address,"password":enPassword}).then((result) => {
+    const enPassword = await bcrypt.hash(password, 10)
+    await userModel.create({ "firstName": firstName, "lastName": lastName, "mobileNumber": mobileNumber, "email": email, "address": address, "password": enPassword }).then((result) => {
         res.status(201).send({
-            message:"Registered successfully !!",
-            data:result
+            message: "Registered successfully !!",
+            data: result
         })
     }).catch((err) => {
         res.status(500).send({
-            message:"Something went wrong !!",
-            error:err
+            message: "Something went wrong !!",
+            error: err
         })
     });
 }
 
 
-const userCart=(req,res)=>{
-    
+const userCart = (req, res) => {
+
 }
 
 
 
 
-module.exports={getFunction,userRegister,userSignIn,profileUpload}
+module.exports = { getFunction, userRegister, userSignIn, profileUpload }
 
 
